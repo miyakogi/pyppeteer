d05681475d62d4c35f42e9c9f19bf59ceecf7070
cheng10 <cheng10@ualberta.ca>
2018-02-28 17:13:28 +0800

    fix pyppeteer #27: Request.resourceType never populated

--- pyppeteer/network_manager.py
+++ pyppeteer/network_manager.py
@@ -280,6 +280,7 @@ class Request(object):
         self._completePromise = asyncio.get_event_loop().create_future()
 
         self.url = url
+        self.resourceType = resourceType
         self.method = payload.get('method', '')
         self.postData = payload.get('postData', '')
         self.headers = payload.get('headers', {})
