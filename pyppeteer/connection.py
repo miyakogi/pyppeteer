9ee4bf62f8c6374d34b954311b8a85ca2ed5d426
scp10011 <506775557@qq.com>
2019-04-10 12:12:26 +0800

    禁用ping

--- pyppeteer/connection.py
+++ pyppeteer/connection.py
@@ -41,7 +41,8 @@ class Connection(EventEmitter):
         self.connection: CDPSession
         self._connected = False
         self._ws = websockets.client.connect(
-            self._url, max_size=None, loop=self._loop)
+            self._url, max_size=None, loop=self._loop, ping_interval=None, ping_timeout=None,
+            close_timeout=3600)
         self._recv_fut = self._loop.create_task(self._recv_loop())
         self._closeCallback: Optional[Callable[[], None]] = None
 
@@ -310,5 +311,5 @@ def _createProtocolError(error: Exception, method: str, obj: Dict
 
 
 def _rewriteError(error: Exception, message: str) -> Exception:
-    error.args = (message, )
+    error.args = (message,)
     return error
